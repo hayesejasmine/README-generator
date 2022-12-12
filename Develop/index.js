a96a1185e80972a30915129c4c5eef4937e4ae7f
hayesejasmine <hayesejasmine@gmail.com>
2022-12-11 21:42:40 -0500

    index.js updated with questions for user input as well as function to write readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a brief description of your project.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What type of license should your project have?',
+            choices: ['Apache 2.0','GNU General Public v3.0', 'MIT', 'BSD 2-Clause "Simplified"', 'BSD 3-Clause "New" or "Revised"','Boost Software 1.0',
+            'Creative Commons Zero v1.0 Universal','Eclipse Public 2.0','GNU Affero General Public v3.0','GNU General Public v2.0','GNU Lesser General Public v2.1','Mozilla Public 2.0','The Unlicense','None',]
+        },
+        {
+            type: 'input',
+            name: 'dependencies',
+            message: 'What command should be ran to install dependecies?',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What command should be ran to run tests?',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'What does the user need to know about using the repo?',
+        },
+     ])
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    err ? console.log(err) : console.log('Successfully created README!')
+  });
+  }
 
+  
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((input) => {
+        writeToFile('README.md', generateMarkdown(input));
+    }
+    )
+}
 
 // Function call to initialize app
 init();
